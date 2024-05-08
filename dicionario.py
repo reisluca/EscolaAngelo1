2bbb598734dd69490f5a2610469b39b017d25d88
luquinha pika fina <166625879+reisluca@users.noreply.github.com>
2024-05-08 08:20:17 -0300

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,25 @@
+listaNomes = []
+
+listaNomes.append("Edson")
+listaNomes.append("Tamires")
+listaNomes.append("Sofia")
+
+print(listaNomes)
+print(listaNomes[1])
+
+for nome in listaNomes:
+    print(nome)
+
+# Dicionarios 
+
+primeiroDicionario = {}
+
+primeiroDicionario['nome'] = "Edson"
+primeiroDicionario['tipo'] = 'professor'
+primeiroDicionario['numero'] = 9
+print(primeiroDicionario)
+print(primeiroDicionario["tipo"])
+
+#anatomia 
+#lista[indice] = Valor 
+dicionario[chave] = Valor 
